1127ea25c604c669b359ff723c385c8de6aef718
Iryna Shyrobokova <iv.shyrobokova@gmail.com>
2024-11-01 20:44:17 -0400

    login works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { CartComponent } from './components/cart/cart.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' }, // Redirect root to login
-  { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
   { path: 'cart', component: CartComponent },
-  { path: '**', redirectTo: 'login' } 
+  { path: 'login', component: LoginComponent }, // Route for the login page
+  { path: '**', redirectTo: '' } // Redirect unknown paths to home
 ];
 
 @NgModule({
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
@@ -10,7 +11,7 @@ import { AuthService } from '../../services/auth.service';
 export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
 
-  constructor(private fb: FormBuilder, private authService: AuthService) { }
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
@@ -33,5 +34,9 @@ export class LoginComponent implements OnInit {
         }
       });
     }
+  }
+
+  closeLogin() {
+    this.router.navigate(['/']); 
   }
 }
